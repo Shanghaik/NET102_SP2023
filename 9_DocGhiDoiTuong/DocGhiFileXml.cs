6bfa38745fb8f0b60f16221b6b59797099922270
shanghaik168@gmail.com <Shanghaik1998>
2023-02-16 15:42:42 +0700

    xml

--- 9_DocGhiDoiTuong/DocGhiFileXml.cs
+++ 9_DocGhiDoiTuong/DocGhiFileXml.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace _9_DocGhiDoiTuong
 {
     internal class DocGhiFileXml
     {
+        
+        public void GhiVaoFileXml<T>(string path, List<T> list)
+        {
+            if(File.Exists(path)) {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                // Tạo 1 đối tượng serializer với kiểu List<T>
+                TextWriter tw = new StreamWriter(path);
+                serializer.Serialize(tw, list); // Ghi thông tin Từ List vào file
+                tw.Close(); // Ghi xong thì đóng luồng lại
+            }
+            else
+            {
+                Console.WriteLine("File không tồn tại");
+            }
+        }
+        public List<T> DocTuFileXml<T>(string path)
+        {
+            if (File.Exists(path))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+                FileStream fs = new FileStream(path, FileMode.Open);
+                // Taoj 1 File stream để lấy dữ liệu từ đường dẫn file
+                var result = serializer.Deserialize(fs);
+                // Đọc dữ liệu đã được đưa vào stream từ file
+                List<T> list = (List<T>)result; // Ép kiểu để thu được kết quả
+                return list;
+            }
+            else
+            {
+                Console.WriteLine("File không tồn tại"); return null;  
+            }
+        }
     }
 }
--- 9_DocGhiDoiTuong/Program.cs
+++ 9_DocGhiDoiTuong/Program.cs
@@ -8,15 +8,23 @@ namespace _9_DocGhiDoiTuong
         static void Main(string[] args)
         {
             List<SanPham> sanPhams= new List<SanPham>() {
-                new SanPham(){Id = Guid.NewGuid(), TenSP = "Cô ca", GiaSp = 10000, NhaCungCap = "Coca"},
+                new SanPham(){Id = Guid.NewGuid(), TenSP = "Co hats", GiaSp = 10000, NhaCungCap = "Coca"},
                 new SanPham(){Id = Guid.NewGuid(), TenSP = "Pepsi", GiaSp = 10000, NhaCungCap = "PP"},
                 new SanPham(){Id = Guid.NewGuid(), TenSP = "Fanta", GiaSp = 10000, NhaCungCap = "FF"},
                 new SanPham(){Id = Guid.NewGuid(), TenSP = "7Up", GiaSp = 10000, NhaCungCap = "7Up"},
                 new SanPham(){Id = Guid.NewGuid(), TenSP = "Sting", GiaSp = 15000, NhaCungCap = "Sting"}
             };
-            DocGhiFileTxt docGhiFileTxt = new DocGhiFileTxt();
-            //docGhiFileTxt.GhiVaoFileText("Sanpham.txt", sanPhams);
-            List<SanPham> sps = docGhiFileTxt.DocTuFileText("Sanpham.txt");
+            //DocGhiFileTxt docGhiFileTxt = new DocGhiFileTxt();
+            ////docGhiFileTxt.GhiVaoFileText("Sanpham.txt", sanPhams);
+            //List<SanPham> sps = docGhiFileTxt.DocTuFileText("Sanpham.txt");
+            //foreach (var item in sps)
+            //{
+            //    item.InThongTin();
+            //}
+            // Làm với file XML
+            DocGhiFileXml docGhiFileXml = new DocGhiFileXml();
+            //docGhiFileXml.GhiVaoFileXml<SanPham>("Sanpham.xml", sanPhams);
+            List<SanPham> sps = docGhiFileXml.DocTuFileXml<SanPham>("Sanpham.xml");
             foreach (var item in sps)
             {
                 item.InThongTin();
